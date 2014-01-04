ce0a81a129d166403d95dcbc4ecd4ddbc783e3a5
Oscar Bezi <oscar.bezi@gmail.com>
2014-01-03 20:49:27 -0500

    Layout wired to compile.

--- public/js/app.js
+++ public/js/app.js
@@ -3,14 +3,33 @@ app.save = function () {
 
 };
 
+app.editor = CodeMirror.fromTextArea(document.getElementById("texdown-code"), {
+    mode:        "gfm",
+    lineNumbers: "true",
+    theme:       "monokai"
+});
+
 app.compile = function() {
-    var data = $('#source').val();
-    $.post(
-        "/compile",
-        { text: data},
-        function (response) {
-            console.log(response.text);
-        },
-        "json"
-    ); 
+    var data = {};
+    data.text = app.editor.getValue();
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState === 4) {
+            if (request.status === 200) {
+                var response = JSON.parse(request.responseText); 
+                document.getElementById('texdown-preview').innerHTML = response.text; 
+            } else {
+                console.log("Shit's fucked up, mate.");
+            }
+        }
+    }
+    request.open('POST', '/compile', true);
+    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    request.send(JSON.stringify(data));
 };
+
+app.init = function () {
+    document.getElementById('compile-button').onclick = app.compile;
+}
+
+app.init();
