2d36a947885e2296d396e368edcffc6dbb3612c6
Oscar Bezi <oscar.bezi@gmail.com>
2014-01-09 02:44:20 -0500

    New edit layout, removed rename and newfile and packaged them into save.

--- routes/edit.js
+++ routes/edit.js
@@ -2,29 +2,32 @@
 module.exports = function (db) {
     var files = db.get('files');
     return function (req, res) {
-        files.find({"_id": req.params.id}, {}, function (err, docs){
-            if (err || !(docs.length === 1 || docs.length === 0)) {
-                res.render(edit, {"statMesg": "There was an error with the database."});
-                return;
-            }
+        if (req.params.id) {
+            files.find({"_id": req.params.id}, {}, function (err, docs){
+                if (err || !(docs.length === 1 || docs.length === 0)) {
+                    res.render(edit, {"statMesg": "There was an error with the database."});
+                    return;
+                }
 
-            if (docs.length === 0) {
-                res.render(edit, {"statMesg": "File does not exist."});
-                return;
-            }
+                if (docs.length === 0) {
+                    res.render(edit, {"statMesg": "File does not exist."});
+                    return;
+                }
 
-            var doc = docs[0];
-            if (!(req.user.id === doc.owner)) {
-                res.send(edit, {"statMesg": "Sorry, you don't own this file."});
-                return;
-            }
+                var doc = docs[0];
+                if (!(req.user.id === doc.owner)) {
+                    res.send(edit, {"statMesg": "Sorry, you don't own this file."});
+                    return;
+                }
 
-            var data = {};
-            data.filename = doc.name;
-            data.fileid = doc._id;
-            data.filecontent = doc.content;
-            res.render('edit', data);
-            return;
-        });
+                var data = {};
+                data.filename = doc.name;
+                data.fileid = doc._id;
+                data.filecontent = doc.content;
+                res.render('edit', data);
+                return;
+            });
+        }
+        res.render('edit', {});
     };
 };
--- routes/save.js
+++ routes/save.js
@@ -1,21 +1,51 @@
 // POST: /save
 module.exports = function (db) {
     var files = db.get('files'); 
+    var users = db.get('tdusers');
     return function (req, res) {
-        files.find({"_id": req.body.fileid}, {}, function (err, docs){
-            if (err) {
-                res.send("There was an error with the database.");
-                return;
-            }
-            if (docs.length === 0) {
-                res.send(JSON.stringify({ 
-                    "statMesg": "Something screwy happened."
-                }), 400);
-                return;
-            }
-            if (docs.length === 1) {
+        var fileid = req.body.fileid;
+        // if the file doesn't have an id, make it
+        if (!fileid) {
+            // check that file name isn't taken
+            files.find({
+                "name": req.body.filename,
+                "owner": req.userid
+            }, {}, function (err, docs) {
+                if (err || !(docs.length === 0)) {
+                    res.send(JSON.stringify({"statMesg": "That file name is already taken."}, 400));
+                    return;
+                }
+            });
+            var file = {};
+            file.name = req.body.filename;
+            file.tags = [];
+            file.owner = req.userid;
+            file.content = req.body.filecontent;
+
+            files.insert(file);
+            // insert file into user's files
+            // and return fileid
+            files.find({
+                "name": req.body.filename,
+                "owner": req.userid
+            }, {}, function (err, docs) {
+                if (err || !(docs.length === 1)) {
+                    res.send(JSON.stringify({"statMesg": "User not found in database."}, 400));
+                    return;
+                }
+                users.update({"id": req.userid}, {"$push": {"files": docs[0]._id}});
+                res.send(JSON.strigify({
+                    "fileid": docs[0]._id
+                })); 
+            });    
+        } else {
+            files.find({"_id": fileid}, {}, function (err, docs){
+                if (err || !(docs.length === 1)) {
+                    res.send("There was an error with the database.");
+                    return;
+                }
                 var doc = docs[0];
-                if (!(req.user.id === doc.owner)) {
+                if (!(req.userid === doc.owner)) {
                     res.send(JSON.stringify({ 
                         "statMesg": "You don't own this file."
                     }), 400);
@@ -23,22 +53,23 @@ module.exports = function (db) {
                 }
 
                 if (req.body.filecontent) {
-                    file.update({"_id": req.body.fileid}, {$set: {
+                    file.update({"_id": fileid}, {$set: {
                         "content": req.body.filecontent, 
                         "name": req.body.filename
                     }}, function (err) {
                         if (err) {
-                            res.send("Database error.", 400);
+                            res.send(JSON.stringify({ 
+                                "statMesg": "Database error."
+                            }), 400);
                             return;
                         }
-                        res.send("Success."); 
+                        res.send(JSON.strigify({
+                            "fileid": fileid
+                        })); 
                     });
-                    return; 
                 }
-                return;
-            }
-            res.send("There was an error with the database.");
-        });
+            });
+        }
     };
 };
 
