9c68a3c0bb3a59f4f4a462bf472352299052a5a2
Oscar Bezi <oscar.bezi@gmail.com>
2014-01-09 01:37:54 -0500

    Fixed edit

--- routes/edit.js
+++ routes/edit.js
@@ -2,36 +2,29 @@
 module.exports = function (db) {
     var files = db.get('files');
     return function (req, res) {
-        if (req.params.id) {
-            files.find({"_id": req.params.id}, {}, function (err, docs){
-                if (err) {
-                    res.send("There was an error with the database.");
-                    return;
-                }
-                if (docs.length === 0) {
-                    res.render('edit', { "newFile": true });
-                    return;
-                }
-                if (docs.length === 1) {
-                    var doc = docs[0];
-                    if (!(req.user.id === doc.owner)) {
-                        res.render('edit', { 
-                            "error": true,
-                            "err-msg": "Sorry, you don't own this file and the owner hasn't made it public."
-                        });
-                        return;
-                    }
+        files.find({"_id": req.params.id}, {}, function (err, docs){
+            if (err || !(docs.length === 1) || !(docs.length === 0)) {
+                res.send(JSON.stringify({"statMesg": "There was an error with the database."}), 400);
+                return;
+            }
 
-                    var data = {};
-                    data.filename = doc.name;
-                    data.fileid = doc._id;
-                    data.filecontent = doc.content;
-                    res.render('edit', data);
-                    return;
-                }
-                res.send("There was an error with the database.");
-            });
-        }
-        res.render('edit', { "newFile": true });
+            if (docs.length === 0) {
+                res.send(JSON.stringify({"statMesg": "File does not exist."}), 400);
+                return;
+            }
+
+            var doc = docs[0];
+            if (!(req.user.id === doc.owner)) {
+                res.send(JSON.stringify({"statMesg": "Sorry, you don't own this file."}), 400);
+                return;
+            }
+
+            var data = {};
+            data.filename = doc.name;
+            data.fileid = doc._id;
+            data.filecontent = doc.content;
+            res.render('edit', data);
+            return;
+        });
     };
 };
